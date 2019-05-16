a3eaa6ae3b51e98624c9eeecb079197a4c7659f6
Cecile Robert-Michon <cerobert@microsoft.com>
2019-05-16 16:54:13 -0700

    missing echo

--- test/e2e/kubernetes/scripts/time-sync-validate.sh
+++ test/e2e/kubernetes/scripts/time-sync-validate.sh
@@ -14,4 +14,4 @@ elif [[ $RELEASE == "18.04" ]]; then
 fi
 sudo timedatectl status | grep 'RTC in local TZ: no' || exit 1
 sudo systemctl status systemd-timesyncd | grep 'Active: active' || exit 1
-sudo systemctl status systemd-timesyncd | grep 'Status: "Synchronized to time server' || "VM not synchronized"
+sudo systemctl status systemd-timesyncd | grep 'Status: "Synchronized to time server' || echo "VM not synchronized"
