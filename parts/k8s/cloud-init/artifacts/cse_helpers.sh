949bce089d4953f94f99b72dd3ea4a1bd4e8c520
Cecile Robert-Michon <cerobert@microsoft.com>
2019-07-03 10:38:02 -0700

    Update cse_helpers.sh

--- parts/k8s/cloud-init/artifacts/cse_helpers.sh
+++ parts/k8s/cloud-init/artifacts/cse_helpers.sh
@@ -53,6 +53,7 @@ ERR_CIS_ASSIGN_FILE_PERMISSION=112 # Error assigning permission to a file in CIS
 ERR_CIS_COPY_FILE=113 # Error writing a file to disk for CIS enforcement
 ERR_CIS_APPLY_PASSWORD_CONFIG=115 # Error applying CIS-recommended passwd configuration
 
+sort -r /etc/*-release
 OS=$(sort -r /etc/*-release | gawk 'match($0, /^ID(|_LIKE)=(.*)/, a) { print toupper(a[2]); exit }')
 UBUNTU_OS_NAME="UBUNTU"
 RHEL_OS_NAME="RHEL"
